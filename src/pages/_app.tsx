d8147683c20a3601001025e639f6285b72541764
Takumi <nase49946@gmail.com>
2022-11-20 22:48:28 +0900

    provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app';
 import 'modern-css-reset';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const origin =
+    typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : '';
+
+  return (
+    <Auth0Provider
+      domain="dev-ueok3jq02bkdk8cp.us.auth0.com"
+      clientId="JSjy1DkbUhRP4yTHnqAuovUP7jPxr5ba"
+      redirectUri={origin}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 };
 
 export default App;
